70a96076610c4285ef0d906a167bee21b0c7248f
Kota Shinohara <Kota@Kota-no-MacBook-Pro.local>
2018-01-13 16:28:18 +0900

    mailer and autocomplete

--- app/mailers/message.rb
+++ app/mailers/message.rb
@@ -1,13 +1,19 @@
 class Message < ApplicationMailer
 
+  default from: testmeets@example.com
+
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.message.hello.subject
   #
   def hello
-    @greeting = "Hi"
+    @name = name
 
-    mail to: "to@example.org"
+    mail (
+      to: "2111atok@gmail.com",
+      subject: "meets test mail") do |format|
+        format.html
+      end
   end
 end
